29ae740cf0c3b3ab8a732a21af6ded46daf6beca
Ngaur <nayan.gaur@gmail.com>
2017-03-09 14:43:37 +0530

    1. Added profile 'webload'. 2. Added code base for web load tests

---
+++ auto-test-load/src/main/java/org/jabong/com/ApiExample.java
@@ -0,0 +1,14 @@
+package org.jabong.com;
+
+
+public final class ApiExample
+{
+
+  public static void main(final String[] args)
+    throws Exception
+  {
+    System.out.println("main");
+
+  }
+
+}
